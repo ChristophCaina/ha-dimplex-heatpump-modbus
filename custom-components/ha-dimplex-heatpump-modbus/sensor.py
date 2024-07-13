38e25eaee84839d65b76180a6d414dc0ca2b8612
Christoph <Christoph@Caina.de>
2024-07-13 21:12:03 +0200

    Update sensor.py

--- custom-components/ha-dimplex-heatpump-modbus/sensor.py
+++ custom-components/ha-dimplex-heatpump-modbus/sensor.py
@@ -1,1 +1,20 @@
+from __future__ import annotations
 
+import logging
+
+from .const import (
+)
+from homeassistanmt.helpers.entity import Entity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.const import (
+  CONF_NAME,
+  UnitOfEnergy,
+  UnitOfPower,
+  UnitOfTemperature,
+)
+from homeassistant.components.sensor import (
+  SensorDeviceClass,
+  SensorEntity,
+  SensorStateClass,
+)
+from homeassistant.core import HomeAssistant
